caceb19ac24b5729e8958d4c3a1799737735b540
Yitzhak Altif <107390911+Yitzhakaltif@users.noreply.github.com>
2022-09-04 11:42:49 +0300

    ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/yitzhak-altif'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -109,23 +109,15 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Yitzhakaltif'
             >
               Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://docs.google.com/document/d/1jXtfLik9g7lG9QTFgQ2awK7us57KRMmM6cyjp23d6wg'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
