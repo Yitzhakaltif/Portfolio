f507ea9d8d95944a4a1858a20178791cf984c6de
Yitzhak Altif <107390911+Yitzhakaltif@users.noreply.github.com>
2022-09-06 11:45:58 +0300

    updated contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/332ce14e-3a69-44f1-a6e9-207dd299a75d" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or Email me Directly - <a href="isaacaltif123@gmail.com" className="href">Isaacaltif123@gmail.com</a></p>
+                <p className='text-gray-300 py-4'>Submit the form below or Email me Directly - Isaacaltif123@gmail</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
