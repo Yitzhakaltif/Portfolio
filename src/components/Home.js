9df61d8f1d65b878c841919fad2a2090cf24edfa
Yitzhak Altif <107390911+Yitzhakaltif@users.noreply.github.com>
2022-09-06 11:58:55 +0300

    ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,17 +14,10 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I'm a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I'm focused on
-          building responsive full-stack web applications.
+          I'm a full-stack developer! I'm focused on
+          building amazing responsive full-stack web applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
-            </span>
-          </button>
         </div>
       </div>
     </div>
