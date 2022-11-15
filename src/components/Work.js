a87100223be9ca9c2b9e80fa87cb7379122459b6
Yitzhak Altif <107390911+Yitzhakaltif@users.noreply.github.com>
2022-11-15 13:55:23 +0200

    added new feature

--- src/components/Work.js
+++ src/components/Work.js
@@ -53,6 +53,11 @@ const Work = () => {
                 Hangman Game
               </span>
               <div className='pt-8 text-center'>
+              <a href='https://yitzhakaltif.github.io/Hangman/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
                 <a href='https://github.com/Yitzhakaltif/DI_HACKATHON'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
